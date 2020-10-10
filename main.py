ff6f46b9897bcf981f169b25960cd5ed38056699
Yuri <Yuribtr@users.noreply.github.com>
2020-10-10 22:08:12 +0300

    Added functionality to display logins among with display name and quantity of messages. Script will ask for action to be performed. Improved search for conversation by removing prefixes.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import json
 import re
-from time import perf_counter
 import html
 from datetime import datetime
 
@@ -30,17 +29,37 @@ def get_message_types(filename):
     Only for debugging purposes - which message types are presents in JSON export file.
     Not used in this script.
     """
+    result = set()
     with open(filename, encoding='utf-8') as file:
         file = json.load(file)
-        items = file['conversations']
-        text = set()
-        for item in items:
-            for message in item['MessageList']:
-                text.add(message['messagetype'])
-    return text
+        conversations = file['conversations']
+        for conversation in conversations:
+            for message in conversation['MessageList']:
+                result.add(message['messagetype'])
+    return result
 
 
-def get_dialog_by_name(filename, person: str, prefix='8:'):
+def remove_prefixes(login: str):
+    return re.sub("^[\d]+:", "", login)
+
+
+def get_login_names(filename):
+    """
+    Only for debugging purposes - which login names are presents in JSON export file.
+    Not used in this script.
+    """
+    result = set()
+    with open(filename, encoding='utf-8') as file:
+        file = json.load(file)
+        conversations = file['conversations']
+        for conversation in conversations:
+            result.add(f'{remove_prefixes(conversation["id"])}'
+                       f'{"" if conversation["displayName"] is None else " [" + conversation["displayName"] + "]"}'
+                       f' - {len(conversation["MessageList"])}')
+    return result
+
+
+def get_dialog_by_name(filename, person: str):
     """
     Function will extract a conversation between your account and your interlocutor using specified login in
     chronological order with highlighting. Usually Skype export file keeps conversation history for last 2 years.
@@ -51,13 +70,12 @@ def get_dialog_by_name(filename, person: str, prefix='8:'):
     :return:    list of formatted conversations
     """
     result = ['Login name was not found!\n']
-    person = prefix + person
     last_msg_date = None
     with open(filename, 'r', encoding='utf-8') as file:
         file = json.load(file)
         items = file['conversations']
         for item in items:
-            if item['id'] == person:
+            if remove_prefixes(item['id']) == person:
                 result = []
                 for message in item['MessageList']:
                     if not message['messagetype'] in ['RichText', 'Text']:
@@ -80,10 +98,12 @@ def get_dialog_by_name(filename, person: str, prefix='8:'):
 
 
 while True:
-    person = input('Please enter Skype login to get conversation list: ')
-    if person != '':
+    print('\n1. Get chat history by login\n2. List all logins')
+    choice = input('Pls select your choice or print "q", and hit "Enter": ')
+    if choice == '1':
+        person = input('Please enter Skype login to get conversation list: ')
+        print(*get_dialog_by_name('messages.json', person=person), sep='\n')
+    if choice == '2':
+        print(*get_login_names('messages.json'), sep='\n')
+    if choice == 'q':
         break
-start_time = perf_counter()
-print(*get_dialog_by_name('messages.json', person=person), sep='\n')
-end_time = perf_counter()
-print(f"Time taken: {(end_time - start_time) * 1000000} mkS")
